40090951637b85aee157ae3c3eb957d44fef79c0
Jainam <sjainam70@yahoo.com>
2024-07-07 11:46:09 +0530

    chore: Update webpack entry file to use TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-	entry: "./src/index.js",
+	entry: "./src/index.ts",
 	mode: "production", // 'development' or 'production'
 	output: {
 		path: path.resolve(__dirname, "dist"),
